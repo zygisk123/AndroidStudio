bd4ca6e59ff103540f399094db2054a3f58afee5
zygisk123 <85321102+zygisk123@users.noreply.github.com>
2022-09-16 00:10:59 +0300

    GAME OVER UPDATE

--- app/src/main/java/com/example/breakoutgame/GameOver.java
+++ app/src/main/java/com/example/breakoutgame/GameOver.java
@@ -3,7 +3,9 @@ package com.example.breakoutgame;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,18 +18,17 @@ public class GameOver extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_over);
-        int points = getIntent().getExtras().getInt("points");
-       // tvPoints = findViewById(R.id.tvPoints);
-       // tvPoints.setText("" + points);
     }
 
     public void restart(View view) {
-        Intent intent = new Intent(GameOver.this, Game.class);
+        Intent intent = new Intent(GameOver.this, MainActivity.class);
         startActivity(intent);
         finish();
     }
 
     public void exit(View view) {
+        System.exit(0);
         finish();
     }
+
 }
